3da8b1cf18fdab5c6007f32d3d93a8c1959993ff
Roderick <enuke4@gmail.com>
2022-03-01 12:57:25 -0600

    modify app.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-
 import './App.css';
 
 class App extends React.Component {
 
-  state = {
-    advice: ''
-  };
+  state = { advice: '' };
 
-  componentDidMount() {
-    this.quoteOfTheDay();
-  }
+  componentDidMount() { this.quoteOfTheDay(); };
 
   //Fetch data from API
   quoteOfTheDay = () => {
